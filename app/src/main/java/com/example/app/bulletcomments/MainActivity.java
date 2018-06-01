9d02f6ecfa64a3e128c6cba678bb336d4e68f57e
Joyounger <942510346@qq.com>
2018-06-01 14:09:30 +0800

    1 设置3个按钮相应事件 2 调整布局,显示出3个按钮
    Signed-off-by: Joyounger <942510346@qq.com>

--- app/src/main/java/com/example/app/bulletcomments/MainActivity.java
+++ app/src/main/java/com/example/app/bulletcomments/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.app.bulletcomments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +12,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatView mFloatView;
-        mFloatView = new FloatView(getApplicationContext());
-        int floatHeight = mFloatView.getStatusBarHeight();
-        mFloatView.createDanmakuView(1,0, floatHeight);
-        mFloatView.addDanmakuView();
+        Button BtnDanmukuView = (Button)findViewById(R.id.button1);
+        Button BtnDanmukuSurfaceView = (Button)findViewById(R.id.button2);
+        Button BtnDanmukuTextureView = (Button)findViewById(R.id.button3);
+
+        BtnDanmukuView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FloatView mFloatView;
+                mFloatView = new FloatView(getApplicationContext());
+                int floatHeight = mFloatView.getStatusBarHeight();
+                mFloatView.createDanmakuView(1,0, floatHeight);
+                mFloatView.addDanmakuView();
+            }
+        });
+        BtnDanmukuSurfaceView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FloatView mFloatView;
+                mFloatView = new FloatView(getApplicationContext());
+                int floatHeight = mFloatView.getStatusBarHeight();
+                mFloatView.createDanmakuView(2,0, floatHeight);
+                mFloatView.addDanmakuSurfaceView();
+            }
+        });
+        BtnDanmukuTextureView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FloatView mFloatView;
+                mFloatView = new FloatView(getApplicationContext());
+                int floatHeight = mFloatView.getStatusBarHeight();
+                mFloatView.createDanmakuView(3,0, floatHeight);
+                mFloatView.addDanmakuTextureView();
+            }
+        });
     }
 }
